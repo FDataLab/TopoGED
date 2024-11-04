91b2161529ec1425eefb03147dd32c5f7f2b2ca9
RBuck8339 <ronanwb2015@outlook.com>
2024-11-04 09:58:33 -0500

    Testing new model hyperparameters

--- my_utils/dataloader.py
+++ my_utils/dataloader.py
@@ -1,48 +1,34 @@
 import networkx as nx
 import pandas as pd
 import numpy as np
-import pickle as pkl
-import torch
-import time
 
 from sklearn.metrics import roc_auc_score 
-from my_utils.utils import Utils
-import scipy.sparse as sp
 
-class DataLoader():
+class MyDataLoader():
+    # File paths
     edge_list_path = 'data/graphpulse/'
     gs_label_path = 'data/graphpulse/labels/'
 
-    def __init__(self):
-        pass 
 
-
-
-    def process_predictions(self, lstm_predictions):
-        nodes = []
-        edges = []
-        densities = [] 
-
-        # Generate the number of nodes, edges, and density of a graph per each snapshot
-
-        return nodes, edges, densities
-                                                      
-
+    # Read the labels from the GraphPulse datasets
     def read_labels(self, dataset):
-        labels = pd.Series.from_csv(self.gs_label_path + '{}_Label.csv'.format(dataset))
+        labels = pd.read_csv(self.gs_label_path + '{}_Label.csv'.format(dataset), header=None)
         return labels
     
 
+    # Generate the labels for the LSTM-GRU predictions
     def to_labels(self, embeddings):
         pred = []
 
         for i in range(1, len(embeddings)):
+            print(f'{embeddings[i][-1]} vs {embeddings[i - 1][-1]}')
             label = 1 if (embeddings[i][-1] - embeddings[i - 1][-1]) > 0 else 0
             pred.append(label)
 
         return pred
     
 
+    # Load the data from edge list txt file
     def load_data(self, dataset):
         print("INFO: Loading a Graph from `Temporal Graph Classification (TGC)` Category: {}".format(dataset))
         data = []
@@ -51,6 +37,7 @@ class DataLoader():
         uniq_ts_list = np.unique(edgelist_df['Snapshot'])
         print("INFO: Number of unique snapshots: {}".format(len(uniq_ts_list)))
 
+        # Loop over snapshot ids
         for ts in uniq_ts_list:
             # NOTE: this code does not use any node or edge features
             ts_edges = edgelist_df.loc[edgelist_df['Snapshot'] == ts, ['from', 'to']]
@@ -60,7 +47,8 @@ class DataLoader():
         return data
 
 
+    # Compare the predictions and truth using desired metrics
     def compute_results(self, pred, truth):
-        pred = np.array(pred)  # Make it a numpy array because it isn't already
         rocauc = roc_auc_score(pred, truth)
         print(f'The predidctions from the LSTMGRU had an ROCAUC score of: {rocauc}')
+        return rocauc
