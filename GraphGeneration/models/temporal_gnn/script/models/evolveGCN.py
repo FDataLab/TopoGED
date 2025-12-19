a4aa0e3e007362d5e81bd3f249a15963798bcaf9
Bảo Ngô <ngot1@myumanitoba.ca>
2025-12-19 15:37:23 -0600

    Added ROLAND, EvolveGCN, GCLSTM, TGCN, WinGNN

---
+++ GraphGeneration/models/temporal_gnn/script/models/evolveGCN.py
@@ -0,0 +1,14 @@
+import torch
+from torch_geometric_temporal.nn.recurrent import EvolveGCNO
+
+class EvolveGCN(torch.nn.Module):
+    def __init__(self, node_feat_dim, hidden_dim):
+        super(EvolveGCN, self).__init__()
+        self.recurrent = EvolveGCNO(node_feat_dim)
+        self.linear = torch.nn.Linear(node_feat_dim, hidden_dim)
+
+    def forward(self, x, edge_index, edge_weight):
+        h = self.recurrent(x, edge_index, edge_weight)
+        h = F.relu(h)
+        h = self.linear(h)
+        return h
